1ec68a924ebabef55bfa88d524aa61c3ba2b0fb0
Will S <will@othersphere.io>
2025-04-06 14:43:40 -0700

    pulled all bill xml files (I think)

--- repsheet_backend/common.py
+++ repsheet_backend/common.py
@@ -1,4 +1,3 @@
-from httpx import AsyncClient
 import os
 from os import path
 import pandas as pd
@@ -6,6 +5,8 @@ import sqlite3
 from contextlib import contextmanager
 from typing import Optional
 import re
+import httpx
+from tenacity import retry, stop_after_attempt, wait_exponential
 
 PARLIMENTARY_SESSIONS = (
     "44-1",
@@ -26,6 +27,7 @@ DATA_DIR = "repsheet_backend/data"
 EXPORT_DB = "repsheet.sqlite"
 
 VOTES_HELD_TABLE = "votes_held"
+BILLS_TABLE = "bills"
 MEMBER_VOTES_TABLE = "member_votes"
 MEMBERS_TABLE = "members"
 
@@ -50,3 +52,31 @@ def print_table_schema(table_name):
         rows = cursor.fetchall()
         for row in rows:
             print(f"{row[1]}: {row[2]} {'NOT NULL' if row[3] else 'NULL'} {'PRIMARY KEY' if row[5] else ''}")
+
+@retry(stop=stop_after_attempt(10), wait=wait_exponential())
+def download_all_bill_texts(parliament, session, bill_number):
+    bill_dir = path.join(DATA_DIR, "bill_text", str(parliament), str(session), str(bill_number))
+    found = False
+    for reading in (1, 2, 3, 4):
+        for lang in ("-E", "_E"):
+            filename = f"{bill_number}_{reading}/{bill_number}{lang}.xml"
+            filepath = path.join(bill_dir, filename)
+            if path.exists(filepath):
+                # empty file indicates that the file was not found
+                found = path.getsize(filepath) > 0
+                continue
+            for bill_type in ("Private", "Government"):
+                url = f"https://www.parl.ca/Content/Bills/{parliament}{session}/{bill_type}/{bill_number}/{filename}"
+                resp = httpx.get(url)
+                os.makedirs(path.dirname(filepath), exist_ok=True)
+                with open(filepath, "wb") as f:
+                    if resp.status_code == 200:
+                        f.write(resp.content)
+                        print(f"Downloaded {filename} from {url}")
+                        found = True
+                        # break otherwise the next iteration might overwrite the file with an empty file
+                        break
+                    else:
+                        # use an empty file to indicate that the file was not found
+                        f.write(b"")
+    return found

ff1e0be88b04bc3694a4265c570a5992d782d703
josh.stabback@gmail.com <josh.stabback@gmail.com>
2025-04-06 16:28:17 -0700

    hey look some frontend is frontending

--- repsheet_frontend/astro.config.mjs
+++ repsheet_frontend/astro.config.mjs
@@ -7,10 +7,13 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "Repsheet Canada",
+      customCss: ["./src/styles/variables.css", "./src/styles/layout.css"],
       social: {
         github: "https://github.com/RepSheet-info/repsheet",
       },
       sidebar: [],
+      lastUpdated: true,
+      pagination: false,
     }),
   ],
 });

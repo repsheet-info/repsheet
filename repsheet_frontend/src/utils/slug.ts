932a5f82e7f970f5136331064986a755e14212bd
josh.stabback@gmail.com <josh.stabback@gmail.com>
2025-04-06 17:02:38 -0700

    building

--- repsheet_frontend/src/utils/slug.ts
+++ repsheet_frontend/src/utils/slug.ts
@@ -1,7 +1,5 @@
-import { Member } from "../types/db";
+import type { Member } from "../types/db";
 
 export function makeMemberSlug(member: Member) {
-  return `${member["First Name"].toLowerCase()}-${member[
-    "Last Name"
-  ].toLowerCase()}`;
+  return `${member["First Name"]}_${member["Last Name"]}`;
 }

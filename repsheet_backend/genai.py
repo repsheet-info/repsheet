bb6bf3869db259c8861f1d03ca258646d8ab4545
Will S <will@othersphere.io>
2025-04-09 21:03:45 -0700

    finished summary merging code

--- repsheet_backend/genai.py
+++ repsheet_backend/genai.py
@@ -1,12 +1,16 @@
 import asyncio
+import os
 from typing import Optional
 from repsheet_backend.cache import GCSCache
 from repsheet_backend.common import GCP_BILLING_PROJECT, CACHE_BUCKET
 from google import genai
 from google.genai.errors import ClientError
 from tenacity import retry, wait_exponential, stop_after_attempt
+from anthropic import Anthropic
 
 GEMINI_FLASH_2 = "gemini-2.0-flash"
+GEMINI_PRO_2_5 = "gemini-2.5-pro-preview-03-25"
+CLAUDE_SONNET = "claude-3-7-sonnet-latest"
 
 COST_PER_MTOK = {GEMINI_FLASH_2: 0.15}
 
@@ -18,11 +22,14 @@ google_ai = genai.Client(
     vertexai=True, project=GCP_BILLING_PROJECT, location="us-central1"
 )
 
-google_ai_api_semaphore = asyncio.Semaphore(MAX_CONCURRENT_REQUESTS)
+anthropic = Anthropic(
+    api_key=os.environ.get("ANTHROPIC_API_KEY", "none"),
+)
 
-# TODO I messed up the cache key prefix so
-# at some point we should migrate this over to having a "google-ai/" prefix
-google_ai_cache = GCSCache(
+api_semaphore = asyncio.Semaphore(MAX_CONCURRENT_REQUESTS)
+
+# It says "google-ai" but I use it for everything
+genai_cache = GCSCache(
     project=GCP_BILLING_PROJECT,
     cache_bucket=CACHE_BUCKET,
     key_prefix="google-ai/",
@@ -33,7 +40,7 @@ google_ai_cache = GCSCache(
     stop=stop_after_attempt(10),
     wait=wait_exponential()
 )
-def _generate_text(prompt: str, model: str) -> Optional[str]:
+def _generate_text_google(prompt: str, model: str) -> Optional[str]:
     """Generate text using Google Gemini."""
     print(f"Generating text with {model} ({len(prompt)} chars)")
     try:
@@ -44,6 +51,20 @@ def _generate_text(prompt: str, model: str) -> Optional[str]:
         raise e
     print(f"Received response from {model} ({len(response.text or "")} chars)")
     return response.text
+
+
+def _generate_text_anthropic(prompt: str, model: str) -> Optional[str]:
+    """Generate text using Anthropic."""
+    print(f"Generating text with {model} ({len(prompt)} chars)")
+    response = anthropic.messages.create(
+        model=model, 
+        max_tokens=8192,
+        messages=[{"role": "user", "content": prompt}],
+    )
+    result = response.content[0].text # type: ignore
+    print(f"Received response from {model} ({len(result)} chars)")
+    return result
+    
 
 
 def _estimate_cost_usd_input_only(prompt: str, model: str) -> float:
@@ -69,12 +90,12 @@ async def estimate_cost_usd_input_only(
         "model": model,
         "prompt": prompt,
     }
-    cached_response = await google_ai_cache.get(cache_key)
+    cached_response = await genai_cache.get(cache_key)
     if cached_response:
         return cached_response
-    async with google_ai_api_semaphore:
+    async with api_semaphore:
         cost = await asyncio.to_thread(_estimate_cost_usd_input_only, prompt, model)
-    await google_ai_cache.set(cache_key, cost)
+    await genai_cache.set(cache_key, cost)
     return cost
 
 
@@ -84,11 +105,14 @@ async def generate_text(prompt: str, model: str = GEMINI_FLASH_2) -> Optional[st
         "model": model,
         "prompt": prompt,
     }
-    cached_response = await google_ai_cache.get(cache_key)
+    cached_response = await genai_cache.get(cache_key)
     if cached_response:
         return cached_response
-    async with google_ai_api_semaphore:
-        response = await asyncio.to_thread(_generate_text, prompt, model)
+    async with api_semaphore:
+        if model.startswith("claude"):
+            response = await asyncio.to_thread(_generate_text_anthropic, prompt, model)
+        else:
+            response = await asyncio.to_thread(_generate_text_google, prompt, model)
     if response is not None:
-        await google_ai_cache.set(cache_key, response)
+        await genai_cache.set(cache_key, response)
     return response

1dba05f58d6c6ae4c153a75e687c2aee11f19681
josh.stabback@gmail.com <josh.stabback@gmail.com>
2025-04-11 13:35:50 -0700

    add a feedback button

--- repsheet_frontend/astro.config.mjs
+++ repsheet_frontend/astro.config.mjs
@@ -17,6 +17,7 @@ export default defineConfig({
       tableOfContents: false,
       components: {
         PageTitle: "./src/components/overrides/PageTitle.astro",
+        PageFrame: "./src/components/overrides/PageFrame.astro",
       },
     }),
   ],

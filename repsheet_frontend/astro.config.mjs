a651199a16f5a771b27e15b8e88ac03e54abde50
josh.stabback@gmail.com <josh.stabback@gmail.com>
2025-04-14 20:13:52 -0700

    add a sidebar

--- repsheet_frontend/astro.config.mjs
+++ repsheet_frontend/astro.config.mjs
@@ -11,7 +11,54 @@ export default defineConfig({
       social: {
         github: "https://github.com/RepSheet-info/repsheet",
       },
-      sidebar: [],
+      sidebar: [
+        {
+          label: "Home",
+          slug: "canada",
+        },
+        {
+          label: "About",
+          slug: "canada/about",
+        },
+        {
+          label: "Methodology",
+          slug: "canada/methodology",
+        },
+        {
+          label: "Party Leaders",
+          items: [
+            {
+              slug: "canada/representative/maxime_bernier",
+              label: "Maxime Bernier",
+            },
+            {
+              link: "/canada/representative/Yves-François_Blanchet",
+              label: "Yves-François Blanchet",
+            },
+            { slug: "canada/representative/mark_carney", label: "Mark Carney" },
+            {
+              link: "/canada/representative/Elizabeth_May",
+              label: "Elizabeth May",
+            },
+            {
+              link: "/canada/representative/Pierre_Poilievre",
+              label: "Pierre Poilievre",
+            },
+            {
+              link: "/canada/representative/Jagmeet_Singh",
+              label: "Jagmeet Singh",
+            },
+          ],
+        },
+        {
+          label: "Feedback",
+          link: "https://forms.gle/spefTkVXmnWjLS5C8",
+          attrs: {
+            target: "_blank",
+            rel: "noopener noreferrer",
+          },
+        },
+      ],
       lastUpdated: true,
       pagination: false,
       tableOfContents: false,
